3a478bb9d9bbd48652f45662bf0818aef2ff422a
fyllen <751711939@qq.com>
2017-08-18 13:58:49 +0800

    企业信息

--- application/index/controller/Service.php
+++ application/index/controller/Service.php
@@ -13,6 +13,7 @@ use app\index\model\WechatUser;
 use app\index\model\CompanyApplication;
 use app\index\model\Park;
 use  app\index\model\WechatTag;
+use  app\index\model\PropertyServer;
 
 
 //企业服务
@@ -371,7 +372,98 @@ public  function  _checkData($data){
             return $this->fetch();
         }
 
+    /*物业报修*/
+    public function repair(){
 
+        if (IS_POST){
+            $property =new PropertyServer();
+            $res=$property->save($_POST);
+            if ($res){
+
+                return $this->success("报修成功");
+            }else{
+
+                return $this->error("报修失败");
+            }
+
+        }
+        $userid =session("userId");
+        $userinfo=WechatUser::where(['userid'=>$userid])->find();
+        $data =[
+            'name'=>$userinfo['name'],
+            'mobile'=>$userinfo['mobile']
+        ];
+
+        //dump($data);
+        $this->assign('data',$data);
+
+        //return  $this->fetch();
+    }
+    /*保洁服务*/
+    public function clear(){
+        if ($_POST){
+            $property =new PropertyServer();
+            $res=$property->save($_POST);
+            if ($res){
+
+                return $this->success("报修成功");
+            }else{
+
+                return $this->error("报修失败");
+            }
+
+        }
+        $userid =session("userId");
+        $userinfo=WechatUser::where(['userid'=>$userid])->find();
+        $data =[
+            'name'=>$userinfo['name'],
+            'mobile'=>$userinfo['mobile']
+        ];
+
+        //dump($data);
+        $this->assign('data',$data);
+
+        return  $this->fetch();
+    }
+    /*物业报修记录*/
+    public function repairRecord(){
+
+        $list = PropertyServer::where(['type'=>['<',4]])->order('create_time desc')->paginate();
+        int_to_string($list,['type'=>[1=>'空调报修',2=>"电梯报修",3=>"其他报修"]]);
+
+
+        $this->assign('list',$list);
+
+        return $this->fetch();
+    }
+
+    /*保洁服务记录*/
+
+    public function clearRecord(){
+        $list = PropertyServer::where(['type'=>['<',4]])->order('create_time desc')->paginate();
+        int_to_string($list,['type'=>[4=>'保洁记录']]);
+
+        $this->assign('list',$list);
+
+        return $this->fetch();
+
+    }
+    /*物业保洁下拉刷新*/
+    public function listmore(){
+        $type=input('type');
+        $len = input('length');
+        if ($type==1){
+
+            $list = PropertyServer::where(['type'=>['<',4]])->order('create_time desc')->limit($len,6)->paginate();
+            int_to_string($list,['type'=>[1=>'空调报修',2=>"电梯报修",3=>"其他报修"]]);
+        }else{
+
+            $list = PropertyServer::where(['type'=>4])->order('create_time desc')->limit($len,6)->paginate();
+        }
+
+        return $list;
+
+    }
 
 
 
