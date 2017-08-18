ee364b2a30abc3c21e5601f582ecbb066542ef31
fyllen <751711939@qq.com>
2017-08-18 13:52:41 +0800

    企业服务

---
+++ application/index/model/PropertyServer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ztos
+ * Date: 2017/8/18
+ * Time: 上午9:29
+ */
+namespace app\index\model;
+
+use think\Model;
+
+class PropertyServer extends Model
+{
+
+
+
+}
