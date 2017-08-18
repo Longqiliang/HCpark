2ef2fd75001c4e7cd140ce6a67b694c1c013aa3e
whttk <18805822955@163.com>
2017-08-18 15:07:48 +0800

    改回数据库

--- application/database.php
+++ application/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'       => 'hcpark',
     // 用户名
-    'username'       => '123456',
+    'username'       => 'root',
     // 密码
-    'password'       => '',
+    'password'       => '123456',
     // 端口
     'hostport'       => '',
     // 连接dsn
