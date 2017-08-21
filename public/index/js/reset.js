20c7e0fa1ed0225a6aedd27399a71f817066d82e
longqiliang <821599372@qq.com>
2017-08-21 10:21:45 +0800

    “室内保洁"页面添加，公共样式及js修改

--- public/index/js/reset.js
+++ public/index/js/reset.js
@@ -279,3 +279,105 @@ function numlimit(n){
 	};
 
 })(jQuery, window);
+
+//Polyfill object.assign()
+if (!Object.assign) {
+	// 定义assign方法
+	Object.defineProperty(Object, 'assign', {
+		enumerable: false,
+		configurable: true,
+		writable: true,
+		value: function(target) { // assign方法的第一个参数
+			'use strict';
+			// 第一个参数为空，则抛错
+			if (target === undefined || target === null) {
+				throw new TypeError('Cannot convert first argument to object');
+			}
+
+			var to = Object(target);
+			// 遍历剩余所有参数
+			for (var i = 1; i < arguments.length; i++) {
+				var nextSource = arguments[i];
+				// 参数为空，则跳过，继续下一个
+				if (nextSource === undefined || nextSource === null) {
+					continue;
+				}
+				nextSource = Object(nextSource);
+
+				// 获取改参数的所有key值，并遍历
+				var keysArray = Object.keys(nextSource);
+				for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+					var nextKey = keysArray[nextIndex];
+					var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+					// 如果不为空且可枚举，则直接浅拷贝赋值
+					if (desc !== undefined && desc.enumerable) {
+						to[nextKey] = nextSource[nextKey];
+					}
+				}
+			}
+			return to;
+		}
+	});
+}
+/*
+ * 身份证15位编码规则：dddddd yymmdd xx p
+ * dddddd：6位地区编码
+ * yymmdd: 出生年(两位年)月日，如：910215
+ * xx: 顺序编码，系统产生，无法确定
+ * p: 性别，奇数为男，偶数为女
+ *
+ * 身份证18位编码规则：dddddd yyyymmdd xxx y
+ * dddddd：6位地区编码
+ * yyyymmdd: 出生年(四位年)月日，如：19910215
+ * xxx：顺序编码，系统产生，无法确定，奇数为男，偶数为女
+ * y: 校验码，该位数值可通过前17位计算获得
+ *
+ * 前17位号码加权因子为 Wi = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ]
+ * 验证位 Y = [ 1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2 ]
+ * 如果验证码恰好是10，为了保证身份证是十八位，那么第十八位将用X来代替
+ * 校验位计算公式：Y_P = mod( ∑(Ai×Wi),11 )
+ * i为身份证号码1...17 位; Y_P为校验码Y所在校验码数组位置
+ * @param {Object} obj vue实例对象，用于调用toast弹窗
+ */
+function validateIdCard(idCard,obj){
+	//15位和18位身份证号码的正则表达式
+	var regIdCard=/^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/;
+	if(!obj){
+		var obj = jQuery;
+		obj.$toast = function (message,options) {
+			jQuery.toast(message,options);
+		}	
+	}
+	//如果通过该验证，说明身份证格式正确，但准确性还需计算
+	if(regIdCard.test(idCard)){
+		if(idCard.length==18){
+			var idCardWi=new Array( 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ); //将前17位加权因子保存在数组里
+			var idCardY=new Array( 1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2 ); //这是除以11后，可能产生的11位余数、验证码，也保存成数组
+			var idCardWiSum=0; //用来保存前17位各自乖以加权因子后的总和
+			for(var i=0;i<17;i++){
+				idCardWiSum+=idCard.substring(i,i+1)*idCardWi[i];
+			}
+
+			var idCardMod=idCardWiSum%11;//计算出校验码所在数组的位置
+			var idCardLast=idCard.substring(17);//得到最后一位身份证号码
+
+			//如果等于2，则说明校验码是10，身份证号码最后一位应该是X
+			if(idCardMod==2){
+				if(idCardLast=="X"||idCardLast=="x"){
+					console.log("验证成功！");
+				}else{
+					obj.$toast("身份证号码错误！");
+				}
+			}else{
+				//用计算出的验证码与最后一位身份证号码匹配，如果一致，说明通过，否则是无效的身份证号码
+				if(idCardLast==idCardY[idCardMod]){
+					console.log("验证成功！");
+				}else{
+					obj.$toast("身份证号码错误！");
+				}
+			}
+		}
+	}else{
+		obj.$toast("身份证格式不正确!");
+	}
+}
