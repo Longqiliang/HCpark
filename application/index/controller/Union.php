b9f0f67be5d9cf9f1056b3cd1f6656de8dac6e12
shen <18805822955@163.com>
2017-08-28 15:06:20 +0800

    工会联盟接口

---
+++ application/index/controller/Union.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shen
+ * Date: 2017/8/28
+ * Time: 下午2:51
+ */
+namespace app\index\controller;
+use app\index\model\Union as UnionModel;
+use app\index\model\UnionLoabour as UnionLoabourModel;
+
+class Union extends Admin
+{
+    public function index(){
+
+    }
+//通知公告和相关活动写一起，传我type1 type2
+    public function unionList(){
+
+    }
+
+    public function unionDetail(){
+
+    }
+//工会信息
+    public function loabourList(){
+
+    }
+
+    public function loabourDetail(){
+
+    }
+
+
+}
