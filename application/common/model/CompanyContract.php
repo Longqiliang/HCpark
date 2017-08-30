f58b6a5524ad01e545bf220d031026dbc1207e8f
fyllen <751711939@qq.com>
2017-08-30 20:49:41 +0800

    合同统计

---
+++ application/common/model/CompanyContract.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ztos
+ * Date: 2017/8/30
+ * Time: 下午7:48
+ */
+namespace app\common\model;
+
+use think\Model;
+
+class CompanyContract extends Model{
+
+
+
+
+
+
+
+}
