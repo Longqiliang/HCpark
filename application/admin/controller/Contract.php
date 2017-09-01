8c4bfd536d440cd3d197131054f11746c6a3a824
fyllen <751711939@qq.com>
2017-09-01 14:03:05 +0800

    园区后台合同管理

--- application/admin/controller/Contract.php
+++ application/admin/controller/Contract.php
@@ -16,42 +16,88 @@ class Contract extends Admin {
 
 
         $map = [
-            "park_id" => session("user_auth")['park_id']
+            "park_id" => session("user_auth")['park_id'],
+            'status' => 0
         ];
         $list = CompanyContract::where($map)->order("create_time desc")->paginate();
         $this->assign("list",$list);
         return $this->fetch();
     }
     public function add(){
+
         $id = input('id');
         $contract = new CompanyContract();
         if (IS_POST){
-            $data = input('post.');
-            $data['create_time'] = time();
-            $data['park_id'] = session("user_auth")['park_id'];
-            $department = WechatDepartment::where("name",input('company'))->find();
-            if ($department){
-                $data['department_id'] = $department['id'];
-            }
-            $data['img'] = json_encode($data['img']);
-            //return $data;
-            $res = $contract->allowField(true)->save($data);
-            if ($res){
+            if (input("id")){
+                $data = input('post.');
+                $update = [
+                    'img' =>json_encode($data['img']),
+                    'company' =>input('company'),
+                    'type' =>input("type"),
+                    'remark'=>input('remark'),
 
-                $this->success("添加成功");
+                ];
+                $department = WechatDepartment::where("name",input('company'))->find();
+                if ($department){
+                    $update['department_id'] = $department['id'];
+                }
+
+                $res = $contract->where('id',input('id'))->update( $update);
+                if ($res){
+
+                    $this->success("修改成功");
+                }else{
+
+                    $this->error("修改失败");
+                }
             }else{
+                $data = input('post.');
+                $data['create_time'] = time();
+                $data['park_id'] = session("user_auth")['park_id'];
+                $department = WechatDepartment::where("name",input('company'))->find();
+                if ($department){
+                    $data['department_id'] = $department['id'];
+                }
+                $data['img'] = json_encode($data['img']);
+                unset($data['id']);
+                $res = $contract->allowField(true)->save($data);
+                if ($res){
 
-                $this->error("添加失败");
+                    $this->success("添加成功");
+                }else{
+
+                    $this->error("添加失败");
+                }
             }
-            return $data;
+
         }else{
-            $contractInfo = $contract->get("$id");
-
+            $contractInfo = $contract->get($id);
+            $this->assign('img',json_decode($contractInfo['img']));
             $this->assign("info",$contractInfo);
+
+            return $this->fetch();
         }
-
-
-        return $this->fetch();
     }
+    /*删除新闻*/
+    public function moveToTrash() {
+        $ids = input('ids/a');
+        $result = CompanyContract::where('id', 'in', $ids)->update(['status' => -1]);
+
+        if($result) {
+
+            return $this->success('删除成功');
+
+        } elseif(!$result) {
+
+            return $this->error('删除失败');
+        }
+    }
+
+
+
+
+
+
+
 
 }
