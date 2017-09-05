855cbdf020cf40a235f05f7dcb291905163e4b92
shen <18805822955@163.com>
2017-09-05 22:25:43 +0800

    论坛帖子详情模块

--- application/admin/controller/Communication.php
+++ application/admin/controller/Communication.php
@@ -13,13 +13,14 @@ use app\common\model\CommunicateUser;
 use app\common\model\CommunicatePosts;
 use app\common\model\CommunicateComment;
 use app\common\model\WechatUser;
+use think\Db;
 
 //合作交流
 class Communication extends Admin
 {   //主页
     public function index()
     {
-        $paek_id = session('park_id');
+        $paek_id = session('user_auth')['park_id'];
         $CommunicateGroup = new CommunicateGroup();
         $map = [
             'park_id' => $paek_id
@@ -37,27 +38,16 @@ class Communication extends Admin
 
     public function edit()
     {
+
         $cgroup = new CommunicateGroup();
         $data = input('');
         $id = $data['id'];
-        unset($data['id']);
-        $reult = $cgroup->save($data, ['id' => $id]);
-        if ($reult) {
-            return $this->success("成功");
-
-        } else {
-
-            return $this->error("失败");
+        if($id) {
+            $reult = $cgroup->save($data, ['id' => $id]);
+        }else{
+            unset($data['id']);
+            $reult = $cgroup->save($data);
         }
-    }
-
-    public function add()
-    {
-        $cgroup = new CommunicateGroup();
-        $data = input('');
-        $id = $data['id'];
-        unset($data['id']);
-        $reult = $cgroup->save($data, ['id' => $id]);
         if ($reult) {
             return $this->success("成功");
 
@@ -69,6 +59,7 @@ class Communication extends Admin
 
     public function showUsers()
     {
+
         $cuser = new CommunicateUser();
         $user = new  WechatUser();
         if (IS_POST) {
@@ -111,4 +102,62 @@ class Communication extends Admin
             return $this->fetch();
         }
     }
+    public function showposts(){
+        $posts = new CommunicatePosts();
+        $user = new  WechatUser();
+
+
+            $id = input('id');
+            $serch = input('search');
+            if (!empty($serch)) {
+                $map['title'] = array('like', '%' . $serch . "%");
+                $userlist = $user->where($map)->select();
+                $ids = array();
+                foreach ($userlist as $value) {
+                    array_push($ids, $value['userid']);
+                }
+                if (count($ids) > 0) {
+                    $data['user_id'] = array('in', $ids);
+                }
+            }
+            $data['group_id'] = $id;
+            $list = $posts->where($data)->where('status','neq',-1)->paginate();
+
+            int_to_string($list, $map = array('status' => array(-1 => '删除', 1 => '审核中', 2 => '审核成功', 3 => '审核失败')));
+
+            $this->assign('search', $serch);
+            $this->assign('list', $list);
+            return $this->fetch();
+    }
+
+    //逻辑删除
+    public function delete() {
+        $ids = input('ids/a');
+var_dump($ids);exit;
+        $result = CommunicatePosts::where('id', 'in', $ids)->update(['status' => -1]);
+        if($result) {
+            return $this->success('删除成功', url('Communication/showposts'));
+        } elseif(!$result) {
+            return $this->error('删除失败');
+        }
+    }
+
+    public function detail()
+    {
+        $id=input('id');
+        $result=Db::table('tb_communicate_posts')
+            ->alias('p')
+            ->join('__COMMUNICATE_GROUP__ g', 'p.group_id=g.id')
+            ->join('__WECHAT_USER__ u', 'p.user_id=u.userid')
+            ->field('g.group_name,u.name')
+            ->where('p.id','eq',$id)
+            ->find();
+        $res= CommunicatePosts::get($id);
+        $res['img']=json_decode($res['img'],true);
+        $res['group_name']=$result['group_name'];
+        $res['name']=$result['name'];
+        $this->assign('res',$res);
+        return $this->fetch();
+    }
+
 }
