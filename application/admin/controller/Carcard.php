5f6aa165fe5bad27b2b8d0355a5fda3c1d3f6915
zyf <2205446834@qq.com>
2017-09-18 13:28:32 +0800

    zyf 后台车卡，充电柱绑定

--- application/admin/controller/Carcard.php
+++ application/admin/controller/Carcard.php
@@ -15,51 +15,26 @@ class Carcard extends Admin
 {
     public function index()
     {
-        $CardparkRecord = new CarparkRecord();
         $CardparkService = new CarparkService();
         $search = input('search');
         if (!empty($search)) {
-
-
             $map = [
                 'park_card' => array('like', '%' . $search . '%'),
-
+                'status'=>array('neq',-1)
             ];
             $list = $CardparkService->where($map)->order('status asc')->paginate();
-            $ids = array();
-            foreach ($list as $value) {
-                array_push($ids, $value['id']);
-            }
-            unset($map['park_card']);
-            $map['carpark_id'] = array('in', $ids);
-            $map['status'] = array('neq', -1);
-            $list = $CardparkRecord->where($map)->order('status asc')->paginate();
-            $list2 = $CardparkRecord->where($map)->order('status asc')->paginate();
-            int_to_string($list2, array(
-                'status' => array(0 => '审核中', 1 => '审核通过', 2 => '审核失败'),
-                'type' => array(1 => '新卡办理', 2 => '旧卡办理')
-            ));
-            foreach ($list2 as $value) {
-                $value['park_card'] = !empty($value->findService->park_card) ? $value->findService->park_card : "暂无";
-            }
-            $re = $CardparkRecord->where('status', 0)->select();
-            $data['data'] = $list2;
-            $data['num'] = count($re);
         } else {
             $map['status'] = array('neq', -1);
-            $list = $CardparkRecord->where($map)->order('status asc')->paginate();
-            int_to_string($list, array(
-                'status' => array(0 => '审核中', 1 => '审核通过', 2 => '审核失败'),
-                'type' => array(1 => '新卡办理', 2 => '旧卡办理')
-            ));
-            foreach ($list as $value) {
-                $value['park_card'] = !empty($value->findService->park_card) ? $value->findService->park_card : "暂无";
-            }
-            $re = $CardparkRecord->where('status', 0)->select();
-            $data['data'] = $list;
-            $data['num'] = count($re);
+            $list = $CardparkService->where($map)->order('status asc')->paginate();
         }
-
+        int_to_string($list, array(
+            'status' => array(0 => '审核中', 1 => '审核通过', 2 => '审核失败'),
+            'type' => array(1 => '新卡办理', 2 => '旧卡办理')
+        ));
+        $re = $CardparkService->where('status', 0)->select();
+        $data['data'] = $list;
+        $data['num'] = count($re);
+        $this->assign('search',$search);
         $this->assign('list', $data);
         return $this->fetch();
 
@@ -67,9 +42,8 @@ class Carcard extends Admin
 
     public function check()
     {
-        $CardparkRecord = new CarparkRecord();
+        $CardparkService = new CarparkService();
         if (IS_POST) {
-            $CardparkService = new CarparkService();
             $type = input('type');
             $id = input('id');
             $park_card = input('park_card');
@@ -78,33 +52,25 @@ class Carcard extends Admin
                 if (empty($park_card)) {
                     return $this->error("请填写 停车卡号");
                 }
-                $record = $CardparkRecord->where('id', $id)->find();
+                $record = $CardparkService->where('id', $id)->find();
+                $record['park_card'] = $park_card;
                 $record['status'] = 1;
                 $record->save();
-                $service = $CardparkService->where('id', $record['carpark_id'])->find();
-                $service['park_card'] = $park_card;
-                $service->save();
-
                 return $this->success("审核成功");
-
             } //没通过
             elseif ($type == 2) {
-                $record = $CardparkRecord->where('id', $id)->find();
+                $record = $CardparkService->where('id', $id)->find();
+                $record['park_card'] = $park_card;
                 $record['status'] = 2;
                 $record->save();
                 return $this->success("审核成功");
             }
         } else {
             $id = input('id');
-            $info = $CardparkRecord->where('id', $id)->find();
+            $info = $CardparkService->where('id', $id)->find();
             $info['payment_voucher'] = json_decode($info['payment_voucher']);
-            $info['name'] = isset($info->findService->name) ? $info->findService->name : "";
-            $info['mobile'] = isset($info->findService->mobile) ? $info->findService->mobile : "";
-            $info['car_card'] = isset($info->findService->car_card) ? $info->findService->car_card : "";
-            $info['people_card'] = isset($info->findService->people_card) ? $info->findService->people_card : "";
-            $info['user'] = isset($info->findService->user->name) ? $info->findService->user->name : "";
             $info['type'] = $info['type'] == 1 ? "新卡办理" : "旧卡续费";
-            $info['park_card'] = isset($info->findService->park_card) ? $info->findService->park_card : "";
+            $info['user']=isset($info->user->name)?$info->user->name:"";
             switch ($info['status']) {
                 case 0:
                     $info['status_text'] = "审核中";
@@ -116,7 +82,6 @@ class Carcard extends Admin
                     $info['status_text'] = "审核失败";
                     break;
             }
-
             $this->assign('info', $info);
             return $this->fetch();
         }
@@ -124,8 +89,8 @@ class Carcard extends Admin
 
     public function del()
     {
-        $CardparkRecord = new CarparkRecord();
-        $result = $CardparkRecord->where('id', input('id'))->find();
+        $CardparkService = new CarparkService();
+        $result = $CardparkService->where('id', input('id'))->find();
         $result['status'] = -1;
         $result->save();
         if ($result) {
@@ -135,28 +100,6 @@ class Carcard extends Admin
         }
     }
 
-    public function change()
-    {
-        $CardparkService = new CarparkService();
-        $CardparkRecord = new CarparkRecord();
-        $id = input('id');
-        $park_card = input('park_card');
-        if (empty($park_card)) {
-            return $this->error("请填写 停车卡号");
-        }
-        $record = $CardparkRecord->where('id', $id)->find();
-        $map = [
-            'park_card' => $park_card
-        ];
-        $service = $CardparkService->where('id', $record['carpark_id'])->Update($map);
-        if ($service != false) {
-            return $this->success("修改成功");
-        } else {
-            return $this->error("修改失败");
-        }
-
-
-    }
 
 
 }
--- application/admin/controller/Electricity.php
+++ application/admin/controller/Electricity.php
@@ -15,49 +15,38 @@ class Electricity extends Admin
 {
     public function index()
     {
-        $ElectricityRecord = new ElectricityRecord();
+
         $ElectricityService = new ElectricityService();
         $search = input('search');
         if (!empty($search)) {
             $map = [
                 'electricity_id' => array('like', '%' . $search . '%'),
+                'status'=>array('neq',-1)
             ];
             $list = $ElectricityService->where($map)->order('status asc')->paginate();
-            $ids = array();
-            foreach ($list as $value) {
-                array_push($ids, $value['id']);
-            }
-            unset($map['electricity_id']);
-            $map['service_id'] = array('in', $ids);
-            $map['status'] = array('neq', -1);
-            $list = $ElectricityRecord->where($map)->order('status asc')->paginate();
-            $list2 = $ElectricityRecord->where($map)->order('status asc')->paginate();
-            int_to_string($list2, array(
+            int_to_string($list, array(
                 'status' => array(0 => '审核中', 1 => '审核通过', 2 => '审核失败'),
                 'type' => array(1 => '新柱办理', 2 => '旧柱办理')
             ));
-            foreach ($list2 as $value) {
-                $value['electricity_id'] = !empty($value->findService->electricity_id) ? $value->findService->electricity_id : "暂无";
-            }
-            $re = $ElectricityRecord->where('status', 0)->select();
-            $data['data'] = $list2;
+
+            $re = $ElectricityService->where('status', 0)->select();
+            $data['data'] = $list;
             $data['num'] = count($re);
 
 
         } else {
             $map['status'] = array('neq', -1);
-            $list = $ElectricityRecord->where($map)->order('status asc')->paginate();
+            $list = $ElectricityService->where($map)->order('status asc')->paginate();
             int_to_string($list, array(
                 'status' => array(0 => '审核中', 1 => '审核通过', 2 => '审核失败'),
                 'type' => array(1 => '新柱办理', 2 => '旧柱办理')
             ));
-            foreach ($list as $value) {
-                $value['electricity_id'] = !empty($value->findService->electricity_id) ? $value->findService->electricity_id : "暂无";
-            }
-            $re = $ElectricityRecord->where('status', 0)->select();
+
+            $re = $ElectricityService->where('status', 0)->select();
             $data['data'] = $list;
             $data['num'] = count($re);
         }
+        $this->assign('search',$search);
         $this->assign('list', $data);
         return $this->fetch();
 
@@ -65,10 +54,10 @@ class Electricity extends Admin
 
     public function check()
     {
-        $ElectricityRecord = new ElectricityRecord();
+        $ElectricityService = new ElectricityService();
 
         if (IS_POST) {
-            $ElectricityService = new ElectricityService();
+
             $type = input('type');
             $id = input('id');
             $electricity_id = input('electricity_id');
@@ -77,32 +66,34 @@ class Electricity extends Admin
                 if (empty($electricity_id)) {
                     return $this->error("请填写 充电柱编号");
                 }
-                $record = $ElectricityRecord->where('id', $id)->find();
+                $record = $ElectricityService->where('id', $id)->find();
+                //新柱申请
+                if($record['type']==1){
+                    $map['electricity_id']=$electricity_id;
+                    $map['status']=1;
+                    $is_has =$ElectricityService->where($map)->find();
+                    if($is_has){
+                        return $this->error('此柱已有使用者');
+                    }
+                }
                 $record['status'] = 1;
+               $record['electricity_id']=$electricity_id;
                 $record->save();
-                $service = $ElectricityService->where('id', $record['service_id'])->find();
-                $service['status'] = 1;
-                $service['electricity_id'] = $electricity_id;
-                $service->save();
-
                 return $this->success("审核成功");
 
             } //没通过
             elseif ($type == 2) {
-                $record = $ElectricityRecord->where('id', $id)->find();
+                $record = $ElectricityService->where('id', $id)->find();
                 $record['status'] = 2;
                 $record->save();
                 return $this->success("审核成功");
             }
         } else {
             $id = input('id');
-            $info = $ElectricityRecord->where('id', $id)->find();
+            $info = $ElectricityService->where('id', $id)->find();
             $info['payment_voucher'] = json_decode($info['payment_voucher']);
-            $info['name'] = isset($info->findService->name) ? $info->findService->name : "";
-            $info['mobile'] = isset($info->findService->mobile) ? $info->findService->mobile : "";
-            $info['user'] = isset($info->findService->user->name) ? $info->findService->user->name : "";
             $info['type'] = $info['type'] == 1 ? "新柱办理" : "旧柱续费";
-            $info['electricity_id'] = isset($info->findService->electricity_id) ? $info->findService->electricity_id : "";
+            $info['user'] = isset($info->user->name )?$info->user->name:"";
             switch ($info['status']) {
                 case 0:
                     $info['status_text'] = "审核中";
@@ -114,12 +105,10 @@ class Electricity extends Admin
                     $info['status_text'] = "审核失败";
                     break;
             }
-
             $this->assign('info', $info);
             return $this->fetch();
         }
     }
-
     public function del()
     {
         $CardparkRecord = new CarparkRecord();
@@ -134,29 +123,5 @@ class Electricity extends Admin
             $this->error('删除失败');
         }
     }
-
-    public function change()
-    {
-        $ElectricityRecord = new ElectricityRecord();
-        $ElectricityService = new ElectricityService();
-        $id = input('id');
-        $electricity_id = input('electricity_id');
-        if (empty($electricity_id)) {
-            return $this->error("请填写 充电柱编号");
-        }
-        $record = $ElectricityRecord->where('id', $id)->find();
-        $map = [
-            'electricity_id' => $electricity_id
-        ];
-        $service = $ElectricityService->where('id', $record['service_id'])->Update($map);
-        if ($service != false) {
-            return $this->success("修改成功");
-        } else {
-            return $this->error("修改失败");
-        }
-
-
-    }
-
 
 }
