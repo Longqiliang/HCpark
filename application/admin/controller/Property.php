5e1e71574748b617eb0ab18a97a2be1fd1482f8c
zyf <2205446834@qq.com>
2017-10-16 09:44:35 +0800

    zyf  后台（物业报修，改状态的文字）

--- application/admin/controller/Property.php
+++ application/admin/controller/Property.php
@@ -18,7 +18,7 @@ class Property extends Admin
         $parkid =session("user_auth")['park_id'];
         $list=PropertyServer::where(['park_id'=> $parkid,'type'=>['<',4],'status'=>['in',[0,1,2]]])->order('create_time desc')->paginate();
         int_to_string($list,['type'=>[1=>'空调报修',2=>"电梯报修",3=>"其他报修"]]);
-        int_to_string($list,['status'=>[0=>"进行中",1=>"已完成",2=>"审核失败"]]);
+        int_to_string($list,['status'=>[0=>"进行中",1=>"审核成功",2=>"审核失败",3=>'服务完成']]);
         $this->assign("list",$list);
 
         return $this->fetch();
