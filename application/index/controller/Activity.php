0159d63ba0d4049a70df61a109dcfcb2170560e2
zyf <2205446834@qq.com>
2017-12-13 09:45:35 +0800

    zyf 活动报名控制器初建

---
+++ application/index/controller/Activity.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ztos
+ * Date: 2017/12/13
+ * Time: 上午9:41
+ */
+
+namespace app\index\controller;
+
+
+class Activity extends Base
+{
+    //活动报名主页
+    public function index()
+    {
+
+
+        $this->fetch();
+    }
+
+    //活动报名详情页
+    public function detail()
+    {
+
+
+        $this->fetch();
+    }
+
+    //报名
+    public function signUp()
+    {
+
+
+        return $this->fetch();
+    }
+
+
+}
