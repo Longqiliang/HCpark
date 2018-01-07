1baf41e1191859ddcc879b912137fa7d52452f43
zyf <2205446834@qq.com>
2018-01-07 17:17:39 +0800

    zyf

--- application/admin/controller/Trademark.php
+++ application/admin/controller/Trademark.php
@@ -118,7 +118,7 @@ class Trademark extends Admin
         $info['name'] = isset($info->user->name) ? $info->user->name : "";
         $info['mobile'] = isset($info->user->mobile) ? $info->user->mobile : "";
         $info['submit_img'] = json_decode($info['submit_img']);
-        $info['back_img'] = json_decode($info['back_img'])[0];
+        $info['back_img'] = json_decode($info['back_img']);
         if ($info['status'] == 1) {
             $info['status_text'] = "已联系";
         } else {
@@ -126,19 +126,17 @@ class Trademark extends Admin
         }
         $this->assign('info', $info);
         return $this->fetch();
-
-
     }
 
     //商标查询回复
     public function reply()
     {
         $id = input('id');
-        $back_img = input('back_img');
-
+        $reply=input('reply');
+        $back_img =input('back_img');
         $ti = new TrademarkInquire();
-        $ta = $ti->get($id);
-        $res = $ti->where('id', $id)->update(['back_img' => json_encode([$back_img]), 'status' => 1, 'end_time' => time()]);
+        $ta = $ti->where('id',$id)->find();
+        $res = $ti->where('id', $id)->update(['back_img' => $back_img, 'status' => 1, 'end_time' => time(),'reply'=>$reply]);
         if ($res) {
            // 商标查询服务回复提示/时间/您的商标查询园区已回复，点击查看详情；
             $message = [
