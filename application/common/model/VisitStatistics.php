13ededa2f1f4ab59cff7272041e973d774d4142e
zyf <2205446834@qq.com>
2018-01-23 14:23:43 +0800

    zyf 首页数据统计折线

---
+++ application/common/model/VisitStatistics.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ztos
+ * Date: 2018/1/19
+ * Time: 下午4:10
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class VisitStatistics extends  Model
+{
+
+
+
+}
